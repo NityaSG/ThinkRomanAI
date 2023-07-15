224ace45d54f1b36a5a3045b60e25aabc6d34dab
Nitya Singh <76156577+NityaSG@users.noreply.github.com>
2023-07-15 20:13:30 +0530

    first commit

---
+++ src/views/index-sections/Chat.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Chat() {
+  return (
+    <div >
+      <h1 style={{marginLeft:10,marginTop:2+'em',color:'black'}}>TrCare Symptom Checker</h1>
+      <h5 style={{marginLeft:10,marginTop:1+'em',border:1}}>Click bottom blue icon for the access</h5>
+    <h3 style={{marginLeft:10}}>Introducing TrCARE: Your AI Symptom Checker
+
+TrCARE is an advanced AI-powered symptom checker designed to provide you with accurate and reliable healthcare guidance. With TrCARE, you gain access to a cutting-edge solution that harnesses the power of artificial intelligence to help you understand and evaluate your symptoms with ease and convenience.</h3>
+    </div>
+  )
+}
+
+export default Chat
